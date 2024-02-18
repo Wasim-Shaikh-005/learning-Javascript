615636031632dedeb53a6376b70919ea18152eda
Wasim-Shaikh-005 <159995333+Wasim-Shaikh-005@users.noreply.github.com>
2024-02-18 16:30:56 +0000

    nums and Math

---
+++ 01_basics/08_nums-and-math.js
@@ -0,0 +1,47 @@
+// const score =400
+// console.log(score)
+
+// const balance= new Number(100) //to give number data type manually
+// //type on console to see properties
+
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3))
+
+// const otherNumber= 23.8966
+
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString())
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+//------------------------------MATH--------------------------
+
+
+// console.log(Math)
+
+// console.log(Math.abs(-4))//converts neg value to pos but not vice versa
+
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.8))
+// console.log(Math.min(4,3,6,8))
+// console.log(Math.max(4,3,6,8))
+
+
+//---------------------------------------------------------------
+
+console.log(Math.random())
+console.log((Math.random()*10)+1) //to get a number between 1 and 10
+console.log(Math.floor((Math.random()*10))+1) 
+
+const min= 6
+const max=15
+
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)//IMPORTANT
+//Formula to get a random number between a range
+
